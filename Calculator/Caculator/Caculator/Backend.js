9b8b54a8f66ba110be1f1c92e15135bbefdbce97
prasadtelkikar <telkikar.prasad@gmail.com>
2017-10-11 23:52:08 +0530

    Started writing javascript code;started writing validation

---
+++ Calculator/Caculator/Caculator/Backend.js
@@ -0,0 +1,14 @@
+﻿function DisplayResult() {
+    var value = document.getElementById("extra7").value;
+    console.log(value);
+    alert(value);
+}
+
+function isNumber(evt) {
+    evt = (evt) ? evt : window.event;
+    var charCode = (evt.which) ? evt.which : evt.keyCode;
+
+    if (charCode > 31 && (charCode < 48 || charCode > 57))
+        return false;
+    return true;
+}
